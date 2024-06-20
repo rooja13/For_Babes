dafc1341d1ce1131615a71c7e0b08e7d71a54367
rooja13 <rbarajas1633@yahoo.com>
2024-06-20 00:38:42 -0700

    Heart Background HTML and CSS Implemented
    -Moving heart background has been implemented.
    
    -Next is to implement text and buttons

---
+++ for_babe/src/components/letter.js
@@ -0,0 +1,19 @@
+import './letter.css'
+
+function LoveLetter(){
+    return(
+        <>
+        <div id='app'>
+            <div id='heart-container'>
+                <div id='heart-pattern'>
+                    <div id='heart-overlay'>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        </>
+    );
+}
+
+export default LoveLetter;
