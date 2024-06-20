4f1885430887452a44ce1f67e9fb7099198694f2
rooja13 <rbarajas1633@yahoo.com>
2024-06-20 15:29:04 -0700

    Messages and Window Created
    -Window overlaying the background was created
    -Non-functional buttons have been implemented
    -Text was implemented
    
    -Next is to add Spotify API to play songs

--- for_babe/src/components/letter.js
+++ for_babe/src/components/letter.js
@@ -10,7 +10,25 @@ function LoveLetter(){
                     </div>
                 </div>
             </div>
-            
+            <div id='window-wrapper'>
+                <div id='window'>
+                    <div id='window-background'>
+                        <div id='window-contents'>
+                            <div id='window-message'>
+                                <h1 class="sixty-four-font">Hi Baby &hearts;</h1>
+                                <p class="sixty-four-font">Welcome to your web page!
+                                     I wanted to make this for you to show how much I love you. 
+                                     I hope ypu like it and be sure to press the buttons :&#41;
+                                </p>
+                            </div>
+                            <div id='window-actions'>
+                                <button type='button' class='window-action'>Change the Music</button>
+                                <button type='button' class='window-action'>Open a Letter</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     );
